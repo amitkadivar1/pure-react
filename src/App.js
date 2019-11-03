e81bd090917297116c58864755afae341c32528a
amit <amit.kadivar1@gmail.com>
2019-11-04 00:58:50 +0530

    add props in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-const Car =()=>{
+// Pure React Concept 
+const Car =({name,model,price})=>{
+    // Destructore Concept
     return React.createElement('div',{},[
-    React.createElement('h1',{},'HondaCity'),
-     React.createElement('h2',{},'2016'),
-     React.createElement('h2',{},'10L'),
+    React.createElement('h1',{},name),
+     React.createElement('h2',{},model),
+     React.createElement('h2',{},price),
     ])
 }
 
 const App = ()=>{
     return React.createElement('div',{},[
-        React.createElement("h1",{},'Car Detail'),
-        React.createElement(Car)
+        React.createElement("h1",{},'Adopt Me!'),
+        React.createElement(Car,{name:'HondaCity',model:'2016',price:'10L'}),
+        React.createElement(Car,{name:'Fortuner',model:'2019',price:'26L'})
     ])
 }
 
